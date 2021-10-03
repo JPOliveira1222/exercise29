0610ca4414d528356b6cb04600df697361ab7425
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:42:04 -0400

    pseudocode for 29

---
+++ src/main/java/exercise29/solution29.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 29 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise29;
+
+import java.util.*;
+
+public class solution29 {
+
+    /*
+    Set a boolean to test conditions for a true/false statement
+
+    in a do/while loop continue asking for user input unless given and integer
+
+    if the user inputs 0 or a Character,
+    catch the exceptions and print "sorry. that's not a valid input"
+
+    if the user inputs a digit besides 0 run it through the equation (72/user input)
+
+    print "It will take " +user input+ " years to double your investment."
+
+     */
+
+
+    
+}
+
+
+
+
